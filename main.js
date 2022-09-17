bf986f44cdd4f1caea7ba2ba5f5eec185ea591dc
Shritha-Unicorn-Tech <82032032+Shritha-Unicorn-Tech@users.noreply.github.com>
2022-09-17 17:43:25 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+function setup(){
+    video = createCapture(VIDEO);
+    video.size(550,500);
+
+    canvas = createCanvas(550,500);
+    canvas.position(560,150);
+
+    poseNet = ml5.poseNet(video,modalLoaded);
+    poseNet.on('pose', gotPoses);
+
+}
+
+function modalLoaded(){
+    console.log (' poseNet is Initialized!');
+}
+
+function gotPoses(results)
+{
+    if (results.length> 0){
+        console.log(results);
+    }
+}
+
+function draw(){
+    background('#969A97');
+    
+}
